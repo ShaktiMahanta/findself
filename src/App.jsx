92e50656e974b7c4da13f28ec170ded45cc01df5
shaktiranjanmahanta <shakti003009@gmail.com>
2025-09-30 20:58:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css"; // or the path to your CSS file
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl text-red-500 font-sans">Simple app</h1>
+      <h1 className="bg-yellow-300 text-blue-600 text-3xl p-4">
+        Test Tailwind
+      </h1>
+      <h1 className="text-red-500">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
